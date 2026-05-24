5b3ea2d0fd4953e8491c39b1ec8a6b0f07633038
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-05-24 17:53:26 +0700

    Add api/mahwa.js

---
+++ api/mahwa.js
@@ -0,0 +1,16 @@
+import { komikuPustaka } from './utils.js';
+
+export default async function handler(req, res) {
+  try {
+    const data = await komikuPustaka({ tipe: "manhwa" });
+    const mapped = data.map(c => ({
+      title: c.title,
+      link: c.url,
+      thumbnail: c.image,
+      genre: c.type
+    }));
+    res.status(200).json(mapped);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
